3d9e5e8e49bf03b3ef5b5e0117da4bfc0ccf6cef
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-17 10:35:32 +0700

    Initial commit and setup project

---
+++ src/main/java/zuhaproject/monitoring/StudySpringMonitoringApplication.java
@@ -0,0 +1,13 @@
+package zuhaproject.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zuhaproject/monitoring/StudySpringMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package zuhaproject.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
